b7a908c58694bcab6396eba2221ffa0d47d7f7d0
Jose Ede de Campos Neto <C0604381@vale.com>
2019-12-17 20:33:57 -0300

    Refatora e faz passar os testes básicos com 1 algarismo

---
+++ src/test/java/dojo3/NumeroRomanoTest.java
@@ -0,0 +1,100 @@
+package dojo3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumeroRomanoTest {
+
+    @Test
+    public void para_I_deveRetornar_1() {
+        String entrada = "I";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(1, retorno);
+    }
+
+    @Test
+    public void para_II_deveRetornar_2() {
+        String entrada = "II";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(2, retorno);
+    }
+
+
+    @Test
+    public void para_IV_deveRetornar_4() {
+        String entrada = "IV";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(4, retorno);
+    }
+
+    @Test
+    public void para_XIV_deveRetornar_14() {
+        String entrada = "XIV";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(14, retorno);
+    }
+
+    @Test
+    public void para_V_deveRetornar_5() {
+        String entrada = "V";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(5, retorno);
+    }
+
+    @Test
+    public void para_X_deveRetornar_10() {
+        String entrada = "X";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(10, retorno);
+    }
+
+    @Test
+    public void para_MMM_deveRetornar_3000() {
+        String entrada = "MMM";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(3000, retorno);
+    }
+
+
+    static class NumeroRomano {
+
+        private final static Map<String, Integer> map = new HashMap<String, Integer>();
+
+        static {
+            map.put("M", 1000);
+            map.put("X", 10);
+            map.put("V", 5);
+            map.put("I", 1);
+        }
+
+        public static int converter(String entrada) {
+
+            int valor = 0;
+
+            if(map.containsKey(entrada)){
+                valor = map.get(entrada);
+            }
+
+            return valor;
+        }
+    }
+
+}
