86913f0182c2e45aaef3c60c1af20a404a2f7ed9
Jose Ede de Campos Neto <C0604381@vale.com>
2019-12-17 21:22:33 -0300

    Adiciona o restos dos maps e casos de testes do exercicio

--- src/test/java/dojo3/NumeroRomanoTest.java
+++ src/test/java/dojo3/NumeroRomanoTest.java
@@ -91,14 +91,55 @@ public class NumeroRomanoTest {
         assertEquals(225, retorno);
     }
 
+    @Test
+    public void para_DCCCXLV_deveRetornar_845() {
+        String entrada = "DCCCXLV";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(845, retorno);
+    }
+
+    @Test
+    public void para_MMXXII_deveRetornar_2022() {
+        String entrada = "MMXXII";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(2022, retorno);
+    }
+    
+    @Test
+    public void para_MMDCLXVI_deveRetornar_2666() {
+        String entrada = "MMDCLXVI";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(2666, retorno);
+    }
+    
+    @Test
+    public void para_MCMXCIX_deveRetornar_1999() {
+        String entrada = "MCMXCIX";
+
+        int retorno = NumeroRomano.converter(entrada);
+
+        assertEquals(1999, retorno);
+    }
+
     static class NumeroRomano {
 
         private final static Map<String, Integer> map = new HashMap<String, Integer>();
 
         static {
             map.put("M", 1000);
+            map.put("CM", 900);
+            map.put("D", 500);
+            map.put("CD", 400);
             map.put("C", 100);
+            map.put("XC", 90);
             map.put("L", 50);
+            map.put("XL", 40);
             map.put("X", 10);
             map.put("IX", 9);
             map.put("V", 5);
